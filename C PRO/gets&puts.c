ab013ecae0b1e2e51ada8375f422536c2896f8d4
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-07 08:17:56 +0530

    Add files via upload

---
+++ C PRO/gets&puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[30];
+    printf("enter a name: ");
+    gets(name);
+    printf("name: ");
+    puts(name);
+    return 0;
+}
