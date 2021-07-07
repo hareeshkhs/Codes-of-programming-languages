4160e8e00ee5273985e99470240bba3238d79a64
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-07 08:13:47 +0530

    Add files via upload

---
+++ C PRO/chapter1.c
@@ -0,0 +1,23 @@
+Book Name:Let Us C
+**********************************************************************************
+CHAPTER-1:GETTING STARTER.
+EXERCISE:-
+**********************************************************************************
+1.gross salary of employee.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float bs,d,r,gs;
+    printf("enter bs:");
+    scanf("%f",&bs);
+    d=(40*bs)/100;
+    r=(20*bs)/100;
+    gs=(bs)-(d+r);
+    printf("gross salary is :%.f\n",gs);
+    return 0;
+}
+*********************************************************************************
+
+
