67d03a14397d2d740b40ff91a5788ddc1b1fde2a
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-11 09:42:28 +0530

    Add files via upload

---
+++ C++ PRO/ridding.cpp
@@ -0,0 +1,27 @@
+//function ridding
+*****************************************************************
+#include <iostream>
+using namespace std;
+class Parent
+{
+public:
+void print()
+{
+cout << "The Parent print function was called" << endl;
+}
+};
+class Child : public Parent
+{
+public:
+void print()
+{
+cout << "The child print function was called" << endl;
+} };
+int main() {
+Parent obj1;
+Child obj2 = Child();
+obj1.print();
+obj2.print();
+return 0;
+}
+
