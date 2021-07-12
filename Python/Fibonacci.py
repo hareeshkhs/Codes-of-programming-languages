a94258428779fdd38506e6373da235ad4bcc2cba
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-12 11:55:34 +0530

    Add files via upload

---
+++ Python/Fibonacci.py
@@ -0,0 +1,38 @@
+# Program to display the Fibonacci sequence up to n-th term
+
+nterms = int(input("How many terms? "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
+
+#method 2
+def FibRecursion(n):  
+   if n <= 1:  
+       return n  
+   else:  
+       return(FibRecursion(n-1) + FibRecursion(n-2))  
+ nterms = int(input("Enter the terms? "))  # take input from the user
+  
+if nterms <= 0:  # check if the number is valid 
+   print("Please enter a positive integer")  
+else:  
+   print("Fibonacci sequence:")  
+   for i in range(nterms):  
+       print(FibRecursion(i))
