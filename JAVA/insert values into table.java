9468fe20d9317ac70abe5089408487a025fe0ea8
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-16 08:31:10 +0530

    Add files via upload

---
+++ JAVA/insert values into table.java
@@ -0,0 +1,60 @@
+import java.sql.*;
+public class FirstExample {
+   // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";  
+   static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+
+   //  Database credentials
+   static final String USER = "ASD";
+   static final String PASS = "asd";
+   
+   public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+      System.out.println("Creating statement...");
+      stmt = conn.createStatement();
+      String sql;
+      sql = "SELECT id, first, last, age FROM Employees";
+      ResultSet rs = stmt.executeQuery(sql);
+      while(rs.next()){
+         //Retrieve by column name
+         int id  = rs.getInt("id");
+         int age = rs.getInt("age");
+         String first = rs.getString("first");
+         String last = rs.getString("last");
+
+         //Display values
+         System.out.print("ID: " + id);
+         System.out.print(", Age: " + age);
+         System.out.print(", First: " + first);
+         System.out.println(", Last: " + last);
+      }
+      rs.close();
+      stmt.close();
+      conn.close();
+   }catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+   }catch(Exception e){
+      //Handle errors for Class.forName
+      e.printStackTrace();
+   }finally{
+      try{
+         if(stmt!=null)
+            stmt.close();
+      }catch(SQLException se2){
+      }
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }
+   }
+   System.out.println("Goodbye!");
+ }
+}
