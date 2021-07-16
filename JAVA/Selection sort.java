4c61b82bf77722ee09241b00f99d6d97adf13ff6
CHINTHA HAREESH KUMAR <46935937+hareeshkhs@users.noreply.github.com>
2021-07-16 08:36:42 +0530

    Add files via upload

---
+++ JAVA/Selection sort.java
@@ -0,0 +1,74 @@
+public class SelectionSortExample {  
+    public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }  
+       
+    public static void main(String a[]){  
+        int[] arr1 = {9,14,3,2,43,11,58,22};  
+        System.out.println("Before Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+        System.out.println();  
+          
+        selectionSort(arr1);//sorting array using selection sort  
+         
+        System.out.println("After Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+    }  
+}  
+
+2)
+import java.util.Scanner;  
+  
+public class SelectionSortExample2  
+{  
+   public static void main(String args[])  
+   {  
+       int size, i, j, temp;  
+       int arr[] = new int[50];  
+       Scanner scan = new Scanner(System.in);  
+         
+       System.out.print("Enter Array Size : ");  
+       size = scan.nextInt();  
+         
+       System.out.print("Enter Array Elements : ");  
+       for(i=0; i<size; i++)  
+       {  
+           arr[i] = scan.nextInt();  
+       }  
+         
+       System.out.print("Sorting Array using Selection Sort Technique..\n");  
+       for(i=0; i<size; i++)  
+       {  
+           for(j=i+1; j<size; j++)  
+           {  
+               if(arr[i] > arr[j])  
+               {  
+                   temp = arr[i];  
+                   arr[i] = arr[j];  
+                   arr[j] = temp;  
+               }  
+           }  
+       }  
+         
+       System.out.print("Now the Array after Sorting is :\n");  
+       for(i=0; i<size; i++)  
+       {  
+           System.out.print(arr[i]+ "  ");  
+       }  
+   }  
+} 
